605f0381d7cc4aad624a79a77700a3a48b37ebf9
Augustin Le Fèvre <aulefevre@kilix.fr>
2019-01-03 18:23:31 +0100

    Ex 3.2

--- src/ex3.rs
+++ src/ex3.rs
@@ -42,7 +42,7 @@ fn parse_claim<'a>(claim: &'a str) -> Claim<'a> {
     }
 }
 
-fn get_covered_fabric(claim: Claim, mut fabric: HashMap<String, u16>) -> HashMap<String, u16> {
+fn get_covered_fabric(claim: &Claim, mut fabric: HashMap<String, u16>) -> HashMap<String, u16> {
     let mut x = claim.coordinates.0;
     while x < claim.coordinates.0 + claim.size.0 {
         let mut y = claim.coordinates.1;
@@ -60,15 +60,20 @@ fn get_covered_fabric(claim: Claim, mut fabric: HashMap<String, u16>) -> HashMap
 fn get_covered_fabric_for_claims(
     mut claims: Vec<Claim>,
     fabric: HashMap<String, u16>,
-) -> HashMap<String, u16> {
+) -> (HashMap<String, u16>, Vec<Claim>) {
     return match claims.pop() {
-        Some(claim) => get_covered_fabric_for_claims(claims, get_covered_fabric(claim, fabric)),
-        None => fabric,
+        Some(claim) => {
+            let (fabric, mut claims) =
+                get_covered_fabric_for_claims(claims, get_covered_fabric(&claim, fabric));
+            claims.push(claim);
+            return (fabric, claims);
+        }
+        None => (fabric, Vec::new()),
     };
 }
 
 fn find_fabric_for_claims(claims: Vec<Claim>) -> u32 {
-    let fabric = get_covered_fabric_for_claims(claims, HashMap::new());
+    let (fabric, _) = get_covered_fabric_for_claims(claims, HashMap::new());
     let mut count = 0;
     for (_, &times_covered) in fabric.iter() {
         if times_covered >= 2 {
@@ -76,6 +81,49 @@ fn find_fabric_for_claims(claims: Vec<Claim>) -> u32 {
         }
     }
     return count;
+}
+
+fn is_claim_overlapped(claim: &Claim, fabric: &HashMap<String, u16>) -> bool {
+    let mut is_overlapped = false;
+    let mut x = claim.coordinates.0;
+    while x < claim.coordinates.0 + claim.size.0 {
+        let mut y = claim.coordinates.1;
+        while y < claim.coordinates.1 + claim.size.1 {
+            let key = x.to_string() + "," + &y.to_string();
+            if *fabric.get(&key).unwrap() != 1 {
+                is_overlapped = true
+            }
+            y += 1;
+        }
+        x += 1;
+    }
+    return is_overlapped;
+}
+
+fn find_claim_with_no_overlap<'a>(claims: Vec<Claim<'a>>) -> &'a str {
+    let (fabric, claims) = get_covered_fabric_for_claims(claims, HashMap::new());
+
+    for claim in claims {
+        let is_overlapped = is_claim_overlapped(&claim, &fabric);
+
+        if !is_overlapped {
+            return claim.id;
+        }
+    }
+
+    panic!("No overlapping claim found");
+}
+
+pub fn solve_exercise_2() -> String {
+    let content = fs::read_to_string("./inputs/input3").unwrap();
+    let split_claims = content.trim().lines();
+    let mut claims = Vec::new();
+    for claim in split_claims {
+        claims.push(claim.to_string());
+    }
+    let claims: Vec<Claim> = claims.iter().map(|claim| parse_claim(claim)).collect();
+
+    return String::from(find_claim_with_no_overlap(claims));
 }
 
 pub fn solve_exercise_1() -> u32 {
@@ -95,8 +143,37 @@ mod test {
     use super::*;
 
     #[test]
+    fn solve_exercise_3_2() {
+        assert_eq!(String::from("346"), solve_exercise_2());
+    }
+
+    #[test]
     fn solve_exercise_3_1() {
         assert_eq!(107043, solve_exercise_1())
+    }
+
+    #[test]
+    fn find_claim_with_no_overlap_test() {
+        assert_eq!(
+            "3",
+            find_claim_with_no_overlap(vec![
+                Claim {
+                    id: "1",
+                    coordinates: (1, 3),
+                    size: (4, 4)
+                },
+                Claim {
+                    id: "2",
+                    coordinates: (3, 1),
+                    size: (4, 4)
+                },
+                Claim {
+                    id: "3",
+                    coordinates: (5, 5),
+                    size: (2, 2)
+                },
+            ])
+        )
     }
 
     #[test]
@@ -146,11 +223,11 @@ mod test {
         assert_eq!(
             map,
             get_covered_fabric(
-                Claim {
+                &(Claim {
                     id: "123",
                     coordinates: (3, 2),
                     size: (3, 2)
-                },
+                }),
                 HashMap::new()
             )
         )
