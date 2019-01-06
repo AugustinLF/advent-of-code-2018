34d25cf2b92fbb206124354ebee11dcfb9061ca0
Augustin Le Fèvre <aulefevre@kilix.fr>
2019-01-06 15:37:38 +0100

    Ex4.2

--- src/ex4.rs
+++ src/ex4.rs
@@ -57,12 +57,10 @@ fn rec_get_guards_presence<'a>(
 
     match records.pop() {
         Some(record) => {
-            // handle the guard that wakes up after 1am?
             match record.record_type {
                 RecordType::NewGuard(id) => {
                     current_guard_id = Some(id);
                     presences.entry(id).or_insert(map);
-                    current_asleep_date = Some(record.date);
                 }
                 RecordType::FallsAsleep => {
                     current_asleep_date = Some(record.date);
@@ -93,14 +91,30 @@ fn rec_get_guards_presence<'a>(
     }
 }
 
-fn get_guards_presence(input: &str) -> u32 {
+fn get_presence_from_input(input: &str) -> Presences {
     let presences: Presences = HashMap::new();
     let mut records: Vec<Record> = input.trim().lines().map(parse_record).collect();
     records.sort_by_key(|record| record.date);
     records.reverse();
 
-    let presence = rec_get_guards_presence(records, presences, None, None);
+    rec_get_guards_presence(records, presences, None, None)
+}
 
+fn get_sleepiest_guard_minute(guard_presence: &GuardPresence) -> u32 {
+    let mut minutes_slept: HashMap<u32, u8> = HashMap::new();
+    let mut sleepiest_minute = (0, 0);
+    for &date_time in guard_presence.keys() {
+        let minute_count = minutes_slept.entry(date_time.minute()).or_insert(0);
+        *minute_count += 1;
+        if *minute_count > sleepiest_minute.1 {
+            sleepiest_minute = (date_time.minute(), *minute_count);
+        }
+    }
+    return sleepiest_minute.0;
+}
+
+fn get_sleepiest_guards_minute(input: &str) -> u32 {
+    let presence = get_presence_from_input(input);
     let guards_asleep_time: HashMap<u16, u16> =
         presence.keys().fold(HashMap::new(), |mut map, id| {
             let guard_presence = presence.get(id).unwrap();
@@ -129,22 +143,44 @@ fn get_guards_presence(input: &str) -> u32 {
             }
         },
     );
-    let mut minutes_slept: HashMap<u32, u8> = HashMap::new();
-    let mut sleepiest_minute = (120, 0);
-    for &date_time in presence.get(&sleepy_guard_id).unwrap().keys() {
-        let minute_count = minutes_slept.entry(date_time.minute()).or_insert(0);
-        *minute_count += 1;
-        if *minute_count > sleepiest_minute.1 {
-            sleepiest_minute = (date_time.minute(), *minute_count);
-        }
-    }
-    let sleepiest_minute = sleepiest_minute.0;
+
+    let sleepiest_minute = get_sleepiest_guard_minute(presence.get(&sleepy_guard_id).unwrap());
     return sleepiest_minute * u32::from(sleepy_guard_id);
+}
+
+fn get_most_likely_alseep_minute(input: &str) -> u32 {
+    let presence = get_presence_from_input(input);
+    let minute = presence
+        .iter()
+        .fold(
+            HashMap::new(),
+            |mut guard_map, (&guard_id, guard_presence)| {
+                guard_map.insert(guard_id, get_sleepiest_guard_minute(guard_presence));
+                guard_map
+            },
+        )
+        .iter()
+        .fold(
+            (0, 0), // (id, minute)
+            |(answer_id, answer_minute), (&id, &minute)| {
+                if minute > answer_minute {
+                    (id, minute)
+                } else {
+                    (answer_id, answer_minute)
+                }
+            },
+        );
+    return u32::from(minute.0) * minute.1;
 }
 
 pub fn exercise_4_1() -> u32 {
     let content = fs::read_to_string("./inputs/input4").unwrap();
-    return get_guards_presence(&content);
+    return get_sleepiest_guards_minute(&content);
+}
+
+pub fn exercise_4_2() -> u32 {
+    let content = fs::read_to_string("./inputs/input4").unwrap();
+    return get_most_likely_alseep_minute(&content);
 }
 
 #[cfg(test)]
@@ -154,6 +190,34 @@ mod test {
     #[test]
     fn solve_exercise_4_1() {
         assert_eq!(exercise_4_1(), 4716);
+    }
+
+    #[test]
+    fn solve_exercise_4_2() {
+        assert_eq!(exercise_4_2(), 117061);
+    }
+
+    #[test]
+    fn test_get_sleepiest_minute() {
+        let example = "[1518-11-01 00:00] Guard #10 begins shift
+        [1518-11-01 00:05] falls asleep
+        [1518-11-01 00:25] wakes up
+        [1518-11-01 00:30] falls asleep
+        [1518-11-01 00:55] wakes up
+        [1518-11-01 23:58] Guard #99 begins shift
+        [1518-11-02 00:40] falls asleep
+        [1518-11-02 00:50] wakes up
+        [1518-11-03 00:05] Guard #10 begins shift
+        [1518-11-03 00:24] falls asleep
+        [1518-11-03 00:29] wakes up
+        [1518-11-04 00:02] Guard #99 begins shift
+        [1518-11-04 00:36] falls asleep
+        [1518-11-04 00:46] wakes up
+        [1518-11-05 00:03] Guard #99 begins shift
+        [1518-11-05 00:45] falls asleep
+        [1518-11-05 00:55] wakes up";
+
+        assert_eq!(get_most_likely_alseep_minute(example), 4455);
     }
 
     #[test]
@@ -176,7 +240,7 @@ mod test {
         [1518-11-05 00:45] falls asleep
         [1518-11-05 00:55] wakes up";
 
-        assert_eq!(get_guards_presence(example), 240);
+        assert_eq!(get_sleepiest_guards_minute(example), 240);
     }
 
     #[test]
